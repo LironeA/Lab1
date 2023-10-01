e801d7c44fe7cff6fc5dd188bae5d8a7c3488d37
LironeA <322004@ukr.net>
2023-10-01 16:14:27 +0300

    Lab1 - Done

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
+using Lab1.Core;
+using Lab1.Model;
 
-Console.WriteLine("Hello, World!");
+public static class Program
+{
+    public const string INPUTPATH = @"..\..\..\Input\Input_1.txt";
+    public static void Main()
+    {
+        var inputFile = new Lab1.Model.InputFile(INPUTPATH);
+        Tokenizer.Tokenize(inputFile);
+        Console.WriteLine("Tokens:");
+        inputFile.Tokens.ForEach(token => Console.WriteLine(String.Format("| {0,30} | {1,5} |", token.Value, token.Count)));
+    }
+}
